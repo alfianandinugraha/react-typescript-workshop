6786860d255d4bb362a6a531ae43974f1d2c34de
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-10-25 16:34:41 +0700

    feat(components): add CounterReduce for example of useReducer

--- src/components/CounterReduce.tsx
+++ src/components/CounterReduce.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+
+enum ActionType {
+  INCREMENT,
+  RESET,
+}
+
+type ActionCount =
+  | {
+      type: ActionType.INCREMENT
+      payload: number
+    }
+  | {
+      type: ActionType.RESET
+    }
+  | number
+
+type Reducer = (state: number, action: ActionCount) => number
 
 const CounterReduce = (): React.ReactElement => {
+  const [value, setValue] = useReducer<Reducer>((state, action) => {
+    if (typeof action == 'number') return action
+
+    switch (action.type) {
+      case ActionType.INCREMENT:
+        return state + action.payload
+      case ActionType.RESET:
+        return 0
+      default:
+        return state
+    }
+  }, 0)
+
   return (
     <div>
-      <h1>CounterReduce. Value: </h1>
+      <h1>CounterReduce. Value: {value}</h1>
+      <button onClick={() => setValue(5)}>Set value to 5</button>
+      <button
+        onClick={() => setValue({ type: ActionType.INCREMENT, payload: 2 })}
+      >
+        Increment by 2
+      </button>
+      <button onClick={() => setValue({ type: ActionType.RESET })}>
+        Reset
+      </button>
     </div>
   )
 }
