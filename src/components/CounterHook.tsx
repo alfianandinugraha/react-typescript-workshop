5f5ba67336167e57dfe2c0f941883f4c26d17977
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-10-25 15:46:49 +0700

    feat(hook): add custom hook useCounter

--- src/components/CounterHook.tsx
+++ src/components/CounterHook.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+type CounterState = {
+  count: number
+  setCount: React.Dispatch<React.SetStateAction<CounterState['count']>>
+  incrementBy: (step: number) => void
+}
+
+const useCounter = (initialValue: number = 0): CounterState => {
+  const [count, setCount] = useState(initialValue)
+
+  const incrementBy = (step: number) => {
+    setCount(count + step)
+  }
+
+  return { count, setCount, incrementBy }
+}
 
 const CounterHook = (): React.ReactElement => {
+  const { count, incrementBy } = useCounter(5)
+
   return (
     <div>
-      <h1>Counter Hook. Value: </h1>
+      <h1>Counter Hook. Value: {count}</h1>
+      <button onClick={() => incrementBy(5)}>Increment by 5</button>
     </div>
   )
 }
