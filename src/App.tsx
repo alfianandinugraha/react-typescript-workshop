b1844cf9e7bae030391a6e434e3f224b45cb0d5c
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-10-25 16:08:01 +0700

    feat(init component): init CounterReduce component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { HTMLAttributes } from 'types'
 import Counter from './components/Counter'
 import CounterClass from './components/CounterClass'
 import CounterHook from './components/CounterHook'
+import CounterReduce from './components/CounterReduce'
 
 interface AppProps extends HTMLAttributes {}
 
@@ -17,6 +18,8 @@ function App(props: AppProps) {
       <CounterClass initialCount={5} />
       <hr />
       <CounterHook />
+      <hr />
+      <CounterReduce />
     </>
   )
 }
---
+++ src/components/CounterReduce.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CounterReduce = (): React.ReactElement => {
+  return (
+    <div>
+      <h1>CounterReduce. Value: </h1>
+    </div>
+  )
+}
+
+export default CounterReduce
