54b5685724722ec21a06565553515b528c69c0ac
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-10-26 16:11:32 +0700

    feat(component): add LimitingProps pattern

--- src/advanced/LimitingProps.tsx
+++ src/advanced/LimitingProps.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
 
+type ButtonVariantProps =
+  | {
+      variant: 'primary' | 'secondary'
+      disabled?: boolean
+    }
+  | {
+      variant: 'error'
+    }
+
+type ButtonProps = ButtonVariantProps & React.HTMLAttributes<HTMLDivElement>
+
+const Button = (props: ButtonProps) => {
+  if (props.variant === 'error') return <button>Button {props.variant}</button>
+  return <button disabled={props.disabled}>Button {props.variant}</button>
+}
+
 const LimitingProps = (): React.ReactElement => {
-  return <></>
+  return (
+    <>
+      {/* Only variant 'primary' and 'secondary' have disabled prop's */}
+      <Button variant="error" />
+      <Button variant="primary" disabled />
+      <Button variant="secondary" disabled />
+    </>
+  )
 }
 
 export default LimitingProps
