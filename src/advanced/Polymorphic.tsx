1ec0fe077bfed83ae0206c6d18edaee37de4034a
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-10-26 18:33:33 +0700

    feat(advanced): add Polymorphic pattern

--- src/advanced/Polymorphic.tsx
+++ src/advanced/Polymorphic.tsx
@@ -1,7 +1,42 @@
 import React from 'react'
 
+type TextTag = Extract<keyof JSX.IntrinsicElements, 'p' | 'a' | 'h1' | 'h2'>
+
+type TextProps<T extends TextTag> = {
+  as?: T
+  onClickText?: () => void
+} & JSX.IntrinsicElements[T]
+
+function Text<T extends TextTag>(props: TextProps<T>) {
+  const TagName: React.ElementType = props.as ? props.as : 'p'
+  const tagProps = { ...props }
+  delete tagProps.onClickText
+
+  return (
+    <TagName
+      {...tagProps}
+      onClick={() => props.onClickText && props.onClickText()}
+    />
+  )
+}
+
 const Polymorphic = (): React.ReactElement => {
-  return <></>
+  return (
+    <>
+      <Text as="a" id="string" href="a">
+        Hello from 'a'
+      </Text>
+      <Text
+        as="p"
+        onClickText={() => {
+          console.log('Clicking !')
+        }}
+      >
+        Hello from 'p'
+      </Text>
+      <Text as="h1">Hello from 'p'</Text>
+    </>
+  )
 }
 
 export default Polymorphic
