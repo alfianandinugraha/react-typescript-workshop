0d50c92cffc59dec361f16b440e00e6db7e33754
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-10-26 10:22:24 +0700

    refactor(rename file): rename CounterContext to CounterContextAPI

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { HTMLAttributes } from 'types'
 import Counter from './components/Counter'
 import CounterClass from './components/CounterClass'
-import CounterContextAPI from './components/CounterContext'
+import CounterContextAPI from './components/CounterContextAPI'
 import CounterHook from './components/CounterHook'
 import CounterReduce from './components/CounterReduce'
 
