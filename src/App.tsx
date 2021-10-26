a169956e4affedd515421b9957e0ad91a3fadc35
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-10-26 15:20:12 +0700

    feat(components): init CounterHOC component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { HTMLAttributes } from 'types'
 import Counter from './components/Counter'
 import CounterClass from './components/CounterClass'
 import CounterContextAPI from './components/CounterContextAPI'
+import CounterHOC from './components/CounterHOC'
 import CounterHook from './components/CounterHook'
 import CounterReduce from './components/CounterReduce'
 import Generic from './components/Generic'
@@ -28,6 +29,8 @@ function App(props: AppProps) {
       <Generic<number> value={5} />
       <Generic<string> value="Hello world !" />
       <Generic<number> value={90} />
+      <hr />
+      <CounterHOC />
     </>
   )
 }
---
+++ src/components/CounterHOC.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CounterHOC = (): React.ReactElement => {
+  return <h1 />
+}
+
+export default CounterHOC
