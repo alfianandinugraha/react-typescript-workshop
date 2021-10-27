e59f8960591da177b25cc9076818b32a2df53ce6
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-10-27 10:11:56 +0700

    feat(advanced): add another Overloading function

--- src/advanced/Overloading.tsx
+++ src/advanced/Overloading.tsx
@@ -4,10 +4,25 @@ type HeadingProps = {
   text: string
 }
 
+type AsyncAddCallback = (result: number) => void
+
+function asyncAdd(a: number, b: number): Promise<number>
+function asyncAdd(a: number, b: number, fn: AsyncAddCallback): void
+
+function asyncAdd(a: number, b: number, fn?: AsyncAddCallback) {
+  const result = a + b
+  if (fn) new Promise<number>(() => fn(result))
+  return new Promise<number>((promise, reject) => {
+    promise(result)
+  })
+}
+
 function Heading(): React.ReactElement
 function Heading(props: HeadingProps): React.ReactElement
 
 function Heading(props?: HeadingProps): React.ReactElement {
+  asyncAdd(1, 2, (a) => console.log(a))
+  asyncAdd(2, 5).then((res) => console.log(res))
   return <h3>{props?.text ? `Text: ${props.text}` : 'No Props'}</h3>
 }
 
